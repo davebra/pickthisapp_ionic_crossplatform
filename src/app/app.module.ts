b2163fff38f25b2e18f662391005c98ca8aa900f
Davide <davide@crumpler.com.au>
2018-12-11 12:53:00 +1100

    add camera, continuing add page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
+import { Camera } from '@ionic-native/camera';
 
 import { HomePage } from '../pages/home/home';
 import { FilterPage } from '../pages/home/filter';
@@ -40,6 +41,7 @@ import { GoogleMaps } from '@ionic-native/google-maps';
   ],
   providers: [
     StatusBar,
+    Camera,
     SplashScreen,
     GoogleMaps,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-add',
@@ -7,8 +9,35 @@ import { NavController } from 'ionic-angular';
 })
 export class AddPage {
 
-  constructor(public navCtrl: NavController) {
+  private pictures: string[];
 
+  constructor(
+    public navCtrl: NavController,
+    private camera: Camera,
+    public alertCtrl: AlertController,
+    private DomSanitizer: DomSanitizer
+    ) {
+  }
+
+  onTakePicture() {
+    const options: CameraOptions = {
+      quality: 50,
+      //destinationType: this.camera.DestinationType.DATA_URL,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      //sourceType: Default is CAMERA. PHOTOLIBRARY : 0, CAMERA : 1, SAVEDPHOTOALBUM : 2,
+      allowEdit: true,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      saveToPhotoAlbum: false,
+      cameraDirection: 0
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      //this.pictures.push('data:image/jpeg;base64,' + imageData);
+      this.pictures.push(imageData);
+    }, (err) => {
+        console.log(err);
+      });
   }
 
 }
